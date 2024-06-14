78b48b065ba321e2523f5e5e1b360efe0a32913f
Brown K <refuse1993@gmail.com>
2024-06-14 17:56:41 +0900

    initial-set

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    'nuxt-icon',
+    // '@nuxtjs/supabase',
+    '@pinia/nuxt',
+    '@vite-pwa/nuxt'
+  ],
+  runtimeConfig:{
+    public: {
+      bucketUrl: process.env.BUCKET_URL
+    }
+  },
   shadcn: {
     /**
      * Prefix for all the imported component
@@ -10,5 +22,6 @@ export default defineNuxtConfig({
      * @default "./components/ui"
      */
     componentDir: './components/ui'
-  }
+  },
+devtools: { enabled: true }
 })
